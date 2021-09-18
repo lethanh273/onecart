0405b35d533a13d565b80c180c898b071c5666b9
lethanh273 <letanthanh273@gmail.com>
2021-09-18 13:47:21 +0700

    Update onecart.rb

--- onecart.rb
+++ onecart.rb
@@ -14,23 +14,25 @@ def download_response_csv
 end
 
 def generate_csv_data(variant_ids)
-  # builder headings
-  headings = Variant.joins(:link, :shop).where(variant_id: variant_ids).group(link.platform, link.shop_name).select('link.platform, shop.shop_name').to_a.uniq.map { |t| t.join(':')}
-  csv_heading = ["SKU", headings.flatten ]
+  # build headings
+  shop_platform_headings = Variant.joins(:link, :shop).where(variant_id: variant_ids).group(link.platform, link.shop_name).select('link.platform, shop.shop_name').to_a.uniq.map { |t| t.join(':')}
+  csv_headings = ["SKU", "Quantity", shop_platform_headings.flatten ]
   CSV.generate do |csv|
-  csv << headings
-    all_variants.each do |variant|
-      csv << response_data(headings, variant)
+    csv << csv_headings
+    variant_ids.each do |variant_id|
+      csv << response_data(csv_headings, variant_id)
     end
   end
 end
 
-def response_data(csv_heading,variant)
+def response_data(csv_heading,variant_id)
   result = []
+  variant = Variant.find(variant_id)
   csv_heading.each do |heading|
     data = heading.split(":")
     shop_id = Shop.find(heading[1]).id
-    result << Link.where('platform =? And shop_id = ?', heading[0], shop_id).variant.quantity
+    result << Link.where('platform =? and shop_id = ?', heading[0], shop_id).variant.quantity
   end
-  result
+  quantity_sum = result.inject(0){|sum,x| sum + x }
+  [variant.sku, quantity_sum, result]
 end
